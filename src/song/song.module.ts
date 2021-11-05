9d518af5783cafb285b689c4becc5b678e719a06
taehee <wonderfulhuman@naver.com>
2021-11-06 03:11:58 +0900

    Feat: Song Read API 구현을 위한 Resolver Generate

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SongController } from './song.controller';
 import { SongService } from './song.service';
+import { SongResolver } from './song.resolver';
 
 @Module({
   controllers: [SongController],
-  providers: [SongService]
+  providers: [SongService, SongResolver]
 })
 export class SongModule {}
