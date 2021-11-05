48838b758bc26ed9b0562b037f756ba3bfc3b626
taehee <wonderfulhuman@naver.com>
2021-11-06 03:31:16 +0900

    Test: SongQueryService는 SongResolver에 포함되어야한다.

--- src/song/song.resolver.spec.ts
+++ src/song/song.resolver.spec.ts
@@ -1,18 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SongResolver } from './song.resolver';
+import { Test, TestingModule } from "@nestjs/testing";
+import { SongResolver } from "./song.resolver";
+import { SongQueryService } from "./song.query.service";
 
-describe('SongResolver', () => {
+const mockSongQueryService = {};
+describe("SongResolver", () => {
   let resolver: SongResolver;
-
+  let songQueryService: SongQueryService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SongResolver],
+      providers: [
+        SongResolver,
+        {
+          provide: SongQueryService,
+          useValue: mockSongQueryService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<SongResolver>(SongResolver);
+    songQueryService = module.get<SongQueryService>(SongQueryService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(resolver).toBeDefined();
+  });
+
+  it("should be defined", () => {
+    expect(songQueryService).toBeDefined();
   });
 });
