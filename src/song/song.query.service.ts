394d80b4eed7731d0dbb2840ef8aaf809d98158f
taehee <wonderfulhuman@naver.com>
2021-11-06 03:48:59 +0900

    Feat: songId를 이용하여 Album을 가져오기위한 서비스 로직 작성

--- src/song/song.query.service.ts
+++ src/song/song.query.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { Neo4jService } from "../neo4j/neo4j.service";
 import { Song } from "../song/graph.song.entity";
+import { SONG_TO_ALBUM } from "../relation/relation";
+import { Album } from "../album/graph.album.entity";
 
 @Injectable()
 export class SongQueryService {
@@ -20,6 +22,24 @@ export class SongQueryService {
       const res: Song = {
         songId,
         title,
+      };
+      return res;
+    });
+  }
+
+  async getAlbumBySong(songId: string) {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (song:Song {songId: "${songId}"})-[:${SONG_TO_ALBUM}]-(album)
+             RETURN album
+            `,
+      {}
+    );
+
+    return result.records.map((album) => {
+      const res: Album = {
+        albumId: album.get("album").properties.albumId,
+        name: album.get("album").properties.name,
       };
       return res;
     });
