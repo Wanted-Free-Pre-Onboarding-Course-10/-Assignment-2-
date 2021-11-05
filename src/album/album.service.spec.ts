128a7b2260eaa62a13328798c629191e0be214ee
taehee <wonderfulhuman@naver.com>
2021-11-05 21:56:00 +0900

    Test: 특별한 오류가 없다면 Neo4j에 있는 모든 Album을 반환해야한다.

--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlbumService } from './album.service';
 import { Neo4jService } from '../neo4j/neo4j.service';
+import * as faker from 'faker';
 
-const mockNeo4jService = {};
+const mockNeo4jService = {
+  read: jest.fn(),
+};
+
+jest.mock('neo4j-driver');
 describe('AlbumService', () => {
   let service: AlbumService;
   let neo4jService: Neo4jService;
@@ -28,5 +33,29 @@ describe('AlbumService', () => {
 
   it('should be defined', () => {
     expect(neo4jService).toBeDefined();
+  });
+
+  describe('getAllAlbum', () => {
+    it('특별한 오류가 없다면 Neo4j에 있는 모든 Album을 반환해야한다', async () => {
+      // given
+      const albumRecord = {
+        get: jest.fn().mockReturnValue({
+          properties: {
+            albumId: faker.datatype.uuid(),
+            name: faker.name.title(),
+          },
+        }),
+      };
+
+      neo4jService.read = jest.fn().mockResolvedValueOnce({
+        records: [albumRecord],
+      });
+
+      // when
+      const result = service.getAllAlbum();
+
+      // then
+      await expect(result).resolves.toBeTruthy();
+    });
   });
 });
