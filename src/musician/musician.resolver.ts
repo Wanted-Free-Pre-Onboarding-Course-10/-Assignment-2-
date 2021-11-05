ad5fd64e478287ef7ad34baa2de20fbbbca3c2d1
taehee <wonderfulhuman@naver.com>
2021-11-06 02:44:18 +0900

    Feat: 해당 뮤지션의 모든 앨범 API의 GraphQL Query Resolver 작성

--- src/musician/musician.resolver.ts
+++ src/musician/musician.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Query, Resolver } from "@nestjs/graphql";
 import { MusicianQueryService } from "./musician.query.service";
 import { Musician } from "./graph.musician.entity";
+import { Album } from "../album/graph.album.entity";
 
 @Resolver()
 export class MusicianResolver {
@@ -9,5 +10,12 @@ export class MusicianResolver {
   @Query(() => [Musician])
   async getAllMusician(): Promise<Musician[]> {
     return await this.musicianQueryService.getAllMusician();
+  }
+
+  @Query(() => [Album])
+  async getAlbumByMusician(
+    @Args("musicianId", { type: () => String }) musicianId: string
+  ): Promise<Album[]> {
+    return await this.musicianQueryService.getAlbumByMusician(musicianId);
   }
 }
