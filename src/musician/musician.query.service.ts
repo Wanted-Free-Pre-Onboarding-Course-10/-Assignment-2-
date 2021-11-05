3ac4bef40de1da340e4d98f5dca0abf60db77362
taehee <wonderfulhuman@naver.com>
2021-11-06 02:43:32 +0900

    Feat: 해당 뮤지션의 모든 앨범 API를 가져오는 Service Logic추가

--- src/musician/musician.query.service.ts
+++ src/musician/musician.query.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { Neo4jService } from "../neo4j/neo4j.service";
 import { Musician } from "./graph.musician.entity";
+import { MUSICIAN_TO_SONG, SONG_TO_ALBUM } from "../relation/relation";
+import { Album } from "../album/graph.album.entity";
 
 @Injectable()
 export class MusicianQueryService {
@@ -20,6 +22,24 @@ export class MusicianQueryService {
       const res: Musician = {
         musicianId,
         name,
+      };
+      return res;
+    });
+  }
+
+  async getAlbumByMusician(musicianId: string) {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (musician:Musician {musicianId: "${musicianId}"})-[:${MUSICIAN_TO_SONG}]-(song),
+                (song)-[:${SONG_TO_ALBUM}]-(album)
+              RETURN album`,
+      {}
+    );
+
+    return result.records.map((album) => {
+      const res: Album = {
+        albumId: album.get("album").properties.albumId,
+        name: album.get("album").properties.name,
       };
       return res;
     });
