0770c06394e83ebc2b56d4c35c1e9af27ec05897
taehee <wonderfulhuman@naver.com>
2021-11-06 03:34:59 +0900

    Feat: 모든 Song을 가져오기위한 Service 로직 작성

--- src/song/song.query.service.ts
+++ src/song/song.query.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { Neo4jService } from "../neo4j/neo4j.service";
+import { Song } from "../song/graph.song.entity";
 
 @Injectable()
-export class SongQueryService {}
+export class SongQueryService {
+  constructor(private readonly neo4jService: Neo4jService) {}
+
+  async getAllSong(): Promise<Song[]> {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (song : Song) 
+              RETURN song`,
+      {}
+    );
+
+    return result.records.map((song) => {
+      const songId = song.get("song").properties.songId;
+      const title = song.get("song").properties.title;
+      const res: Song = {
+        songId,
+        title,
+      };
+      return res;
+    });
+  }
+}
