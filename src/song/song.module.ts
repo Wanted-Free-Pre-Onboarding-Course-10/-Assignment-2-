3ada30ef75fe9cf299fd33d2e608c65f6f2b75b5
taehee <wonderfulhuman@naver.com>
2021-11-06 03:23:07 +0900

    Feat: SongQueryService 사용을 위한 SongModule에 추가

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { SongController } from './song.controller';
-import { SongService } from './song.service';
-import { SongResolver } from './song.resolver';
+import { Module } from "@nestjs/common";
+import { SongController } from "./song.controller";
+import { SongService } from "./song.service";
+import { SongResolver } from "./song.resolver";
+import { SongQueryService } from "./song.query.service";
 
 @Module({
   controllers: [SongController],
-  providers: [SongService, SongResolver]
+  providers: [SongService, SongResolver, SongQueryService],
 })
 export class SongModule {}
