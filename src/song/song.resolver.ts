baef9971470c9760c1e6f6068276f9d2d033c9b5
taehee <wonderfulhuman@naver.com>
2021-11-06 03:35:21 +0900

    Feat: 모든 Song을 가져오기위한 GraphQL Query Resolver 작성

--- src/song/song.resolver.ts
+++ src/song/song.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from "@nestjs/graphql";
+import { Song } from "./graph.song.entity";
+import { SongQueryService } from "./song.query.service";
 
 @Resolver()
-export class SongResolver {}
+export class SongResolver {
+  constructor(private songQueryService: SongQueryService) {}
+
+  @Query(() => [Song])
+  async getAllSong(): Promise<Song[]> {
+    return await this.songQueryService.getAllSong();
+  }
+}
