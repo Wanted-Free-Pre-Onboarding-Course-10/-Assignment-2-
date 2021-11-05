8deccc2f7bec7482ba2cf3e40848899d62eac25d
taehee <wonderfulhuman@naver.com>
2021-11-06 03:54:07 +0900

    Feat: songId를 이용하여 Musician의 정보를 가져오기위한 서비스 로직작성

--- src/song/song.query.service.ts
+++ src/song/song.query.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { Neo4jService } from "../neo4j/neo4j.service";
 import { Song } from "../song/graph.song.entity";
-import { SONG_TO_ALBUM } from "../relation/relation";
+import { SONG_TO_ALBUM, SONG_TO_MUSICIAN } from "../relation/relation";
 import { Album } from "../album/graph.album.entity";
+import { Musician } from "../musician/graph.musician.entity";
 
 @Injectable()
 export class SongQueryService {
@@ -40,6 +41,24 @@ export class SongQueryService {
       const res: Album = {
         albumId: album.get("album").properties.albumId,
         name: album.get("album").properties.name,
+      };
+      return res;
+    });
+  }
+
+  async getMusicianBySong(songId: string) {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (song:Song {songId: "${songId}"})-[:${SONG_TO_MUSICIAN}]-(musician)
+             RETURN musician
+            `,
+      {}
+    );
+
+    return result.records.map((album) => {
+      const res: Musician = {
+        musicianId: album.get("musician").properties.musicianId,
+        name: album.get("musician").properties.name,
       };
       return res;
     });
