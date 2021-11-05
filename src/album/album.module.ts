0c9dc806360f3f83e83d825feed362bcadffbb58
taehee <wonderfulhuman@naver.com>
2021-11-05 13:55:24 +0900

    Feat: GraphQL을 위한 Album Service 생성

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AlbumResolver } from './album.resolver';
+import { AlbumService } from './album.service';
 
 @Module({
-  providers: [AlbumResolver]
+  providers: [AlbumResolver, AlbumService]
 })
 export class AlbumModule {}
