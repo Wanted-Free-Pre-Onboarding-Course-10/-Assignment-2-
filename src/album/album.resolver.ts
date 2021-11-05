5fc854b9c61250bd6dd28d2e6723335007a80ea0
taehee <wonderfulhuman@naver.com>
2021-11-05 21:01:59 +0900

    Feat: Graph DB에 존재하는 모든 Album 목록 가져오는 Query 추가
    getAllAlbum Query를 이용하면된다
    
    Schema
    - getAllAlbum: [Album!]!

--- src/album/album.resolver.ts
+++ src/album/album.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Album } from './album.entity';
+import { AlbumService } from './album.service';
 
 @Resolver()
-export class AlbumResolver {}
+export class AlbumResolver {
+  constructor(private albumService: AlbumService) {}
+
+  @Query(() => [Album])
+  async getAllAlbum(): Promise<Album[]> {
+    return await this.albumService.getAllAlbum();
+  }
+}
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Neo4jService } from '../neo4j/neo4j.service';
+import { Album } from './album.entity';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  constructor(private readonly neo4jService: Neo4jService) {}
+
+  async getAllAlbum(): Promise<Album[]> {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (album : Album) 
+              RETURN album`,
+      {},
+    );
+
+    return result.records.map((album) => {
+      const res: Album = {
+        albumId: album.get('album').properties.albumId,
+        name: album.get('album').properties.name,
+      };
+      return res;
+    });
+  }
+}
