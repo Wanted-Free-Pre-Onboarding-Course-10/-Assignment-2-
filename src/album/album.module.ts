ffd249c989a75ccdd82bfb44523211f3145365d9
taehee <wonderfulhuman@naver.com>
2021-11-06 04:28:04 +0900

    Feat: 사용하지않은 QueryResolver 제거

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AlbumController } from "./album.controller";
-import { AlbumQueryResolver } from "./album.query.resolver";
-import { AlbumResolver } from "./album.resolver";
 import { AlbumQueryService } from "./album.query.service";
 import { AlbumService } from "./album.service";
 
 @Module({
   controllers: [AlbumController],
-  providers: [AlbumQueryResolver, AlbumQueryService, AlbumService],
+  providers: [AlbumQueryService, AlbumService],
 })
 export class AlbumModule {}
