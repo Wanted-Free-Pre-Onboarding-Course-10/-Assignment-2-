b94de92b694af189a3b67ba2d15cac5a1d25e447
taehee <wonderfulhuman@naver.com>
2021-11-05 20:48:59 +0900

    Feat: Nest.js가 Module들과 Provider등등의 의존성들을 설정하는 중에 발생하는 이슈를 예방하기위한 Neo4jModule.forRootAsync함수 추가
    Neo4J 도메인이 만약 환경변수를 쓰려고하는데 ConfigModule이 주입이 되지않을수있다.
    
    따라서 Dynamic하고 비동기적으로 Neo4j의 설정을 해줌으로서 해당 이슈를 사전에 예방할수 있다.

--- src/neo4j/neo4j.module.ts
+++ src/neo4j/neo4j.module.ts
@@ -1,4 +1,5 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Neo4jConfig } from 'src/neo4j-config.interface';
 import { NEO4J_CONFIG, NEO4J_DRIVER } from './neo4j.constants';
 import { Neo4jService } from './neo4j.service';
@@ -20,6 +21,27 @@ export class Neo4jModule {
           inject: [NEO4J_CONFIG],
           useFactory: async (config: Neo4jConfig) => createDriver(config),
         },
+      ],
+      exports: [Neo4jService],
+    };
+  }
+
+  static forRootAsync(configProvider: any): DynamicModule {
+    return {
+      module: Neo4jModule,
+      global: true,
+      imports: [ConfigModule],
+      providers: [
+        {
+          provide: NEO4J_CONFIG,
+          ...configProvider,
+        } as Provider<any>,
+        {
+          provide: NEO4J_DRIVER,
+          inject: [NEO4J_CONFIG],
+          useFactory: async (config: Neo4jConfig) => createDriver(config),
+        },
+        Neo4jService,
       ],
       exports: [Neo4jService],
     };
