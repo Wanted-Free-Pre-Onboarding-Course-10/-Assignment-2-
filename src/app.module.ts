6ebe7d294a9b96bfe1bb07c17796e03ddb419af4
taehee <wonderfulhuman@naver.com>
2021-11-05 20:47:44 +0900

    Feat: @nest/config를 통해 민감한 정보를 환경변수화 시켜줌

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,29 @@ import { SongModule } from './song/song.module';
 import { AlbumModule } from './album/album.module';
 import { Neo4jModule } from './neo4j/neo4j.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
+    Neo4jModule.forRootAsync({
+      useFactory: async () => ({
+        scheme: process.env.DB_SCHEME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gpl',
     }),
     MusicianModule,
     SongModule,
     AlbumModule,
-    Neo4jModule,
   ],
   controllers: [],
   providers: [],
