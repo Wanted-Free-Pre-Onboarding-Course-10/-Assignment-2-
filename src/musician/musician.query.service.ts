6ba365450d72179c27430ad137a2b1ba92910bc5
taehee <wonderfulhuman@naver.com>
2021-11-06 02:23:59 +0900

    Feat: 모든 Musician을 가져오는 Service로직 추가

--- src/musician/musician.query.service.ts
+++ src/musician/musician.query.service.ts
@@ -1,60 +1,27 @@
-import { Injectable, Logger } from "@nestjs/common";
-import { Node } from "neo4j-driver-core";
+import { Injectable } from "@nestjs/common";
 import { Neo4jService } from "../neo4j/neo4j.service";
-import { MusicianGraphqlDto } from "./dto/graphql.musician.dto";
-import { MusicianDto } from "./dto/musician.dto";
-import { ResponseMusicianDto } from "./dto/res.musician.dto";
-
-export type Musician = Node;
+import { Musician } from "./graph.musician.entity";
 
 @Injectable()
 export class MusicianQueryService {
-  private logger = new Logger("MusicianService");
   constructor(private readonly neo4jService: Neo4jService) {}
 
-  async createMusician(musicianDto: MusicianDto): Promise<Musician> {
-    const { name, age } = musicianDto;
-
-    const result = await this.neo4jService.write(
-      `CREATE (n: Musician) 
-             SET n += $properties, n.id = randomUUID()
-             RETURN n
-            `,
-      {
-        properties: {
-          name,
-          age,
-        },
-      }
+  async getAllMusician() {
+    const result = await this.neo4jService.read(
+      `MATCH
+                (musician : Musician) 
+              RETURN musician`,
+      {}
     );
 
-    return result.records[0].get("n");
-  }
-
-  async updateMusicianAge(musicianDto: MusicianDto) {
-    const result = await this.neo4jService.write(
-      `MATCH (n {name : $name})
-             SET n.age = $age
-             RETURN n
-            `,
-      {
-        name: musicianDto.name,
-        age: musicianDto.age,
-      }
-    );
-  }
-
-  async deleteMusicianByName(name: string) {
-    const result = await this.neo4jService.write(
-      `MATCH (n {name : $name})
-             DELETE n
-             RETURN n
-            `,
-      {
-        name: name,
-      }
-    );
-
-    this.logger.debug(`deleted node : ${result}`);
+    return result.records.map((album) => {
+      const musicianId = album.get("musician").properties.musicianId;
+      const name = album.get("musician").properties.name;
+      const res: Musician = {
+        musicianId,
+        name,
+      };
+      return res;
+    });
   }
 }
